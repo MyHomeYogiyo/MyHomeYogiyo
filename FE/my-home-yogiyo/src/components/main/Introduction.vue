938a5c35256aa717b9b5a818e853da762b8c2ba1
yehyeon <qkdo12@naver.com>
2023-11-19 22:30:00 +0900

    added keywordSearch page in FE
    Former-commit-id: 35d018eb254eac18f7e6ee3f880bab86e924ff05

--- FE/my-home-yogiyo/src/components/main/Introduction.vue
+++ FE/my-home-yogiyo/src/components/main/Introduction.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ff7b07549642debad515db46ae815a2ff21b5a2c21ef8a88a135dd4d8c10c87
-size 953
+oid sha256:6887207f9d8d92e2b9762e5649739bfed55744bb852a2984267c69a0b44a6478
+size 1071
--- FE/my-home-yogiyo/src/views/KeywordSearchView.vue
+++ FE/my-home-yogiyo/src/views/KeywordSearchView.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <div>아피</div>
+  <div>아파트 이름 검색 페이지</div>
 </template>
 
 <style scoped></style>
