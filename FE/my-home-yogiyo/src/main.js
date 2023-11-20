db4636404b2668d0fb008c630e2a80457c33c3be
권현준 <bonheur9813@naver.com>
2023-11-20 22:38:34 +0900

    🔧 active BE_API_Server_URL in .env & add config pinia-plugin-persistedstate
    Former-commit-id: 9691e68e2f16e540ceaa13cf78edf8191c4c877a

--- FE/my-home-yogiyo/src/main.js
+++ FE/my-home-yogiyo/src/main.js
@@ -1,14 +1,22 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+const app = createApp(App);
+const pinia = createPinia();
 
-app.use(createPinia())
-app.use(router)
+pinia.use(piniaPluginPersistedstate);
 
-app.mount('#app')
+app.use(pinia);
+app.use(router);
+
+router.isReady().then(() => {
+  app.mount("#app");
+});
+
+// app.mount("#app");
