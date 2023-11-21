ab3023730772edab0a8e5e7e035e5fa259d815ac
권현준 <bonheur9813@naver.com>
2023-11-22 01:12:27 +0900

    ✨ feat : registUser - BE
    Former-commit-id: 3d08e60553a47c21a741b48a7da22123a8604df6

--- BE/MyHomeYogiyo/src/main/java/com/ssafy/myhome/notice/model/NoticeDto.java
+++ BE/MyHomeYogiyo/src/main/java/com/ssafy/myhome/notice/model/NoticeDto.java
@@ -1,5 +1,3 @@
-package com.ssafy.myhome.notice.model;
-
-public class NoticeDto {
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:fd5040144822149c77b5a5b3a7e3b79e4e1ecf8a8d0e6c67c492f35451d7ac1e
+size 925
