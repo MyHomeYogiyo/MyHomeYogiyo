492ec814bef36a9c2b0a9a75b77a3f41565ded7f
권현준 <bonheur9813@naver.com>
2023-11-21 10:36:08 +0900

    🎨 update TheHeadingNavbar & index.js for UserMyPage
    Former-commit-id: d36aacfbc723bd9c67d83a8777aa370b20416e67

--- FE/my-home-yogiyo/src/components/layout/TheHeadingNavbar.vue
+++ FE/my-home-yogiyo/src/components/layout/TheHeadingNavbar.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1658fa5dffcf71d84c849e74716c3aa51e0b3709d45acbe14773e69727af525d
-size 2217
+oid sha256:61549f2ec4853c1ce27e141938a6416bfb8cd6cf0c176b813475458ed7df4260
+size 2260
