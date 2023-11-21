673f47108ebd93f5a52700130c40c87ac0109eba
yay <qkdo12@naver.com>
2023-11-21 17:23:30 +0900

    :sparkles: Apt Info Retrieving done into FE from BE
    Former-commit-id: 0670ea0fa8be449c5da629342da0e7b30667d81e

--- FE/my-home-yogiyo/src/App.vue
+++ FE/my-home-yogiyo/src/App.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:86be551d9e48598fd1f90a76bb022d76005172f1dbe20823c041e4ca2c8357f0
-size 369
+oid sha256:dd7ff9a0ac6f7df517aeeb39895f6f5345c59c38ecfa89ff8cae921fbc64eafc
+size 370
--- FE/my-home-yogiyo/src/components/mapsearch/MapSrchRes.vue
+++ FE/my-home-yogiyo/src/components/mapsearch/MapSrchRes.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f04dfa5ba0bff73c427e6275e6f42d26e8c3836216ac9f69723cea83185ed6d
-size 173
+oid sha256:0fc634a6380d5f20648a4ee7fa36c43f1e6f8c2e27daa3f0bf2453d84de23df5
+size 967
