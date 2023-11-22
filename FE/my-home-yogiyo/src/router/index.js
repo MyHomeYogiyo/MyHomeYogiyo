734fb026bf6da18d427ac73d2183274639f4a68f
권현준 <bonheur9813@naver.com>
2023-11-22 10:22:05 +0900

    🎨 modify : add router redirect to user-mypage-userInfo
    Former-commit-id: f5899e6b8a187ff1c22e8c86a3841f2ce4f15abb

--- FE/my-home-yogiyo/src/router/index.js
+++ FE/my-home-yogiyo/src/router/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd60ea2c6ac0668555c7f6510f54e9ae84327271d7162c553b68ba7d2b741afe
-size 2075
+oid sha256:1a7358890a6b95437a99076be871965d95871449d93cfe90a523bb96aa1c6aea
+size 2537
