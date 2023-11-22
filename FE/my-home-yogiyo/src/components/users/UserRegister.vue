7f027106487796a4f24db316498c4ceb1109506a
권현준 <bonheur9813@naver.com>
2023-11-22 10:19:35 +0900

    💄 update UI login & regist & mypage
    Former-commit-id: 96a9c24c83b8529806b373f441176e0b32ff4141

--- FE/my-home-yogiyo/src/components/users/UserRegister.vue
+++ FE/my-home-yogiyo/src/components/users/UserRegister.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb4b197ab69caf9f8084bee6028bfbb5d373ecb497147490962bf03ae11a4390
-size 5966
+oid sha256:b37aeec6251d16ca8dc45ca35ca3ece2f20ce623bae536b20031b91446d48e5c
+size 5999
--- FE/my-home-yogiyo/src/views/TheUserView.vue
+++ FE/my-home-yogiyo/src/views/TheUserView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff065d715170c79b85913accef205f4403ca9ef7690e9c26b42537f9d88735a0
-size 342
+oid sha256:aa6ff57514493fd02ed346eb9423d9ab297d6e97bacf0b10ee7cea9952855674
+size 348
