7d3914574e3fa1ca791108ab90adef435b761b70
권현준 <bonheur9813@naver.com>
2023-11-23 20:35:41 +0900

    💄 change mouse UI when mouse hover on Navbar
    Former-commit-id: 98c57d1cb7459096a50bc25937a960148d2d53a3

--- FE/my-home-yogiyo/src/components/layout/TheHeadingNavbar.vue
+++ FE/my-home-yogiyo/src/components/layout/TheHeadingNavbar.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b12fc8cdccb5ef12b68e1c6061df47da34a4d3ad28aef9ce94a08f09a654be18
-size 2261
+oid sha256:fbdc3b21c0994e342c94d325657bda46f2e96e152caad5f3402dd7d3f628746b
+size 2352
