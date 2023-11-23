a75be608fffe1187f87e26e5a0da10dc30b32eef
권현준 <bonheur9813@naver.com>
2023-11-23 20:37:00 +0900

    🚧 modify password and delete user
    Former-commit-id: af01e348f67efd134dafac04c79c2f5b706b3f15

--- BE/MyHomeYogiyo/src/main/java/com/ssafy/myhome/user/controller/UserController.java
+++ BE/MyHomeYogiyo/src/main/java/com/ssafy/myhome/user/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef752bde3a5ab70a5ef272edc59fe08fd3dfffd14e061a10d1ea4a9f9dd35bd0
-size 6338
+oid sha256:eabd15ac4005c6cd9365bdc6f24b7a9af950b094dc531e5ff26fe991ede27fcb
+size 7899
