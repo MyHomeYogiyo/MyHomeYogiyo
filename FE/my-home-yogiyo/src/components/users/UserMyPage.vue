67d1533c10cab9bc3fe53b4608bdfe0e017af0e9
권현준 <bonheur9813@naver.com>
2023-11-23 20:39:49 +0900

    🚧 modify password and deleteUser at FE
    Former-commit-id: 31f8a0e1d01ee9a25ddedfb49bc1e2ca7563c596

--- FE/my-home-yogiyo/src/components/users/UserMyPage.vue
+++ FE/my-home-yogiyo/src/components/users/UserMyPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd493a050a4df6990c4840973ec82fdab67511f5ef5687ad59911619f128b384
-size 3714
+oid sha256:e972b507b479106f275af2dc443cfbc2902aff1c19cc112bcd928d7f2e44d221
+size 3717
