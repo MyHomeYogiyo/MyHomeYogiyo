b4fabc3cfcdd147f67937cab0cb066f53aedbebd
권현준 <bonheur9813@naver.com>
2023-11-24 12:12:56 +0900

    ✨ feat : view chart
    Former-commit-id: 414a9a03f582daca1581d4c90c27cbd95506d261

--- FE/my-home-yogiyo/src/components/apt/AptListItem.vue
+++ FE/my-home-yogiyo/src/components/apt/AptListItem.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f383ec399e947d445998b03636f9b452d11c48ed15eb69b3d89f85fe69803a0d
-size 7621
+oid sha256:dcea11ec9c936235d70ea51b7297e38117b324f254365b34f3091a7f04f192a6
+size 7896
